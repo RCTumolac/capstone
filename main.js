df9671b2780e2ee757715a5f57921f0b294b83fd
Richard Cole Tumolac <rctumolac@gmail.com>
2017-05-10 16:32:19 +0000

    THINGS

--- main.js
+++ main.js
@@ -2,18 +2,13 @@ $(document).ready(function(){
     $("body").keydown(function(event) {
          var xoffset = $("#player").offset().left - $("#ship").offset().left;
         if (event.which === 37) {
-           
-           $("#player").css("left", xoffset - 10);
+           $("#player").css("left", xoffset - 15);
         } 
-        if(event.which === 38){
-            console.log($("#player").offset().top);
-            $("#player").css("top", $("#player").offset().top - 10);
-        }
+
         if (event.which === 39) {
-           $("#player").css("left", $("#player").offset().left + 10);
+           $("#player").css("left", xoffset + 15);
         } 
-        if(event.which === 40){
-            $("#player").css("top", $("#player").offset().top + 10);
-        }
+
     });
 });
+
